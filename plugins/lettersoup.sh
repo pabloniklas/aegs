fbeb82d801682731145e3d0d5b65b9caf78119e4
Pablo Niklas <pablo.niklas@gmail.com>
2019-10-10 20:27:17 -0300

    ARG Parsing & LaTeX output.

--- plugins/lettersoup.sh
+++ plugins/lettersoup.sh
@@ -7,6 +7,11 @@
 #
 #######################################################################
 #
+# Requirement
+#
+# https://gitlab.com/simers/soup.git
+#
+#
 # ChangeLog: 
 #
 # 09/10/2019 - PSRN - Initial version.
@@ -24,7 +29,7 @@
 declare -A SOUP
 declare -A SOLUTION
 declare -A CHOOSEN_CELLS
-declare SIZE
+declare -a COORDS
 
 #######################################################################
 # Functions area
@@ -34,7 +39,6 @@ LIBPATH=../`dirname $0`/lib
 
 source $LIBPATH/cecho_lib.sh
 
-
 ##################################################################
 ### myFunctions
 ##################################################################
@@ -42,16 +46,89 @@ source $LIBPATH/cecho_lib.sh
 ### print_soup
 function print_soup () {
 
+    local S
+
+    showInfo "Printing SOUP"
+
     for ((I=0;I<$SIZE;I++)) {
+        S=""
         for ((J=0;J<$SIZE;J++)) {
-            echo -n "${SOUP[$I,$J]} "
+            S="$S ${SOUP[$I,$J]} "
         }
-        echo ""
+        showInfo "SOUP[`printf '%0.2d' $I`] => $S"
     }
 }
 
+### print_solution
+function print_solution () {
+
+    local S
+
+    for ((I=0;I<$SIZE;I++)) {
+        S=""
+        for ((J=0;J<$SIZE;J++)) {
+            S="$S ${SOLUTION[$I,$J]} "
+        }
+        showInfo "SOLUTION[`printf '%0.2d' $I`] => $S"
+    }
+}
+
+### soup2latex
+function soup2latex() {
+
+    local ORIENT
+    local A
+    local TEX_OUTPUT
+
+    TEX_OUTPUT="soup.tex"
+
+    echo "" > $TEX_OUTPUT
+    echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%" >> $TEX_OUTPUT
+    echo "% By lettersoup.sh (for AEGS)"  >> $TEX_OUTPUT
+    echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%" >> $TEX_OUTPUT
+    echo "" >> $TEX_OUTPUT
+    echo '\section{Sopa de Letras}' >> $TEX_OUTPUT
+    echo '\question[30] Resolver la sopa de letras.' >> $TEX_OUTPUT
+
+    echo "\\begin{Alphabetsoup}[$SIZE][$SIZE]" >> $TEX_OUTPUT
+
+    for ((N=0;N<${#COORDS[@]};N++)) {
+
+        # Parsing fields
+        W="`echo ${COORDS[$N]}|cut -d: -f1`"
+        X="`echo ${COORDS[$N]}|cut -d: -f2`"
+        X=$(($X+1))
+        Y="`echo ${COORDS[$N]}|cut -d: -f3`"
+        Y=$(($Y+1))
+        O="`echo ${COORDS[$N]}|cut -d: -f4`"
+
+        # Orientation
+        case $O in
+            "H") ORIENT="right" ;;
+            "V") ORIENT="down" ;;
+            "NE") ORIENT="upright" ;;
+            "SO") ORIENT="upleft" ;;
+        esac
+
+        # Each letter separated by comma.
+        A="${W:0:1}"; for((i=1;i<${#W}-1;i++)); do 
+            A="$A,${W:i:1}"
+        done
+        A="$A,${W: -1}"
+
+        echo "\\hideinsoup*{$Y}{$X}{$ORIENT}" >> $TEX_OUTPUT
+        echo "{$A}" >> $TEX_OUTPUT
+        #echo "[]" >> $TEX_OUTPUT
+        echo ""  >> $TEX_OUTPUT
+    }
+    
+    echo "\\end{Alphabetsoup}"  >> $TEX_OUTPUT
+
+    showOK "Written LaTeX output to: $TEX_OUTPUT"
+}
+
 ### For DEV: dev_print_detection_matrix
-function dev_print_detection_matrix {
+function dev_print_detection_matrix () {
 
     echo "========================================"
     for ((X=0;X<$SIZE;X++)) {
@@ -60,6 +137,21 @@ function dev_print_detection_matrix {
         }
         echo ""
     }
+}
+
+### coords2file
+function coords2file () {
+
+    local CFILE
+
+    CFILE=$1
+
+    for ((N=0;N<${#COORDS[@]};N++)) {
+        echo ${COORDS[$N]} >> $CFILE
+    }
+
+    showOK "Written to file $CFILE."
+
 }
 
 ### generate_soup
@@ -85,11 +177,12 @@ function analyze_collision () {
     COL=$4
 
     case $ORIENTATION in
+
         0)  #Horizontal
             for ((J=0;J<${#WORD};J++)) {
                 VCOL=$(($J+$COL))
                 if [ "${CHOOSEN_CELLS[$ROW,$VCOL]}" == "1" ] && [ "${WORD:$J:1}" != ${SOUP[$ROW,$VCOL]} ]; then
-                    echo "WORD $WORD: Colision in ($ROW,$VCOL) (${WORD:$J:1} => ${SOUP[$ROW,$VCOL]}). Relocating."
+                    showWarning "WORD $WORD: Colision in ($ROW,$VCOL) (${WORD:$J:1} => ${SOUP[$ROW,$VCOL]}). Relocating."
                     eval "$5=1"
                     return 
                 fi
@@ -100,7 +193,7 @@ function analyze_collision () {
             for ((J=0;J<${#WORD};J++)) {
                 VROW=$(($J+$ROW))
                 if [ "${CHOOSEN_CELLS[$VROW,$COL]}" == "1" ] && [ "${WORD:$J:1}" != ${SOUP[$VROW,$COL]} ]; then
-                    echo "WORD $WORD: Colision in ($VROW,$COL) (${WORD:$J:1} => ${SOUP[$VROW,$COL]}). Relocating."
+                    showWarning "WORD $WORD: Colision in ($VROW,$COL) (${WORD:$J:1} => ${SOUP[$VROW,$COL]}). Relocating."
                     eval "$5=1"
                     return
                 fi
@@ -120,7 +213,7 @@ function analyze_collision () {
                 done
 
                 if [ "${CHOOSEN_CELLS[$VROW,$VCOL]}" == "1" ] && [ "${WORD:$J:1}" != ${SOUP[$VROW,$VCOL]} ]; then
-                    echo "WORD $WORD: Colision in ($VROW,$VCOL) (${WORD:$J:1} => ${SOUP[$VROW,$VCOL]}). Relocating."
+                    showWarning "WORD $WORD: Colision in ($VROW,$VCOL) (${WORD:$J:1} => ${SOUP[$VROW,$VCOL]}). Relocating."
                     eval "$5=1"
                     return
                 fi
@@ -139,7 +232,7 @@ function analyze_collision () {
                 done
 
                 if [ "${CHOOSEN_CELLS[$VROW,$VCOL]}" == "1" ] && [ "${WORD:$J:1}" != ${SOUP[$VROW,$VCOL]} ]; then
-                    echo "WORD $WORD: Colision in ($VROW,$VCOL) (${WORD:$J:1} => ${SOUP[$VROW,$VCOL]}). Relocating."
+                    showWarning "WORD $WORD: Colision in ($VROW,$VCOL) (${WORD:$J:1} => ${SOUP[$VROW,$VCOL]}). Relocating."
                     eval "$5=1"
                     return
                 fi
@@ -165,8 +258,16 @@ function put_words () {
 
     for WORD in ${WORDS//,/ }; do 
 
+        showInfo "$WORD"
+
         ORIENTATION=$(($RANDOM%4))
         REVERSE=$(($RANDOM%2))
+
+        # Reversed (or not)
+        if [ $REVERSE -eq 1 ]; then 
+            WORD=`echo $WORD| rev`
+            showInfo "    Reversed"
+        fi
 
         case $ORIENTATION in
             0)  ROW=$(($RANDOM%$SIZE))
@@ -214,21 +315,13 @@ function put_words () {
             analyze_collision $WORD $ORIENTATION $ROW $COL RETURN_VAR
         done
 
-        echo -n "WORD: $WORD - "
-
-        # Reversed or not
-        if [ $REVERSE -eq 1 ]; then 
-            WORD=`echo $WORD| rev`
-            echo -n "(rev) - "
-        fi
-
         # Switch orientation
         case $ORIENTATION in
             0)  #Horizontal
+                showInfo "    Orientation: [-] (H) - row: $ROW - col: $COL"
 
-                echo -n "ORIENTATION: [-]  "
-                echo -n "ROW: $ROW - "
-                echo "COL: $COL"
+                COORDS+=("$WORD:$ROW:$COL:H")
+                
                 for ((J=0;J<${#WORD};J++)) {
                     CCOL=$(($J+$COL))
                     SOUP[$ROW,$CCOL]=${WORD:$J:1}
@@ -238,9 +331,9 @@ function put_words () {
                 ;;
 
             1)  #Vertical
-                echo -n "ORIENTATION: [|]. - "
-                echo -n "ROW: $ROW - "
-                echo "COL: $COL"
+                showInfo "    Orientation: [|] (V) - row: $ROW - col: $COL"
+
+                COORDS+=("$WORD:$ROW:$COL:V")
 
                 for ((J=0;J<${#WORD};J++)) {
                     CROW=$(($J+$ROW))
@@ -251,9 +344,9 @@ function put_words () {
                 ;;
 
             2)  #Diagonal NE
-                echo -n "ORIENTATION: [/] (NE). - "
-                echo -n "ROW: $ROW - "
-                echo "COL: $COL"
+                showInfo "    Orientation: [/] (NE) - row: $ROW - col: $COL"
+
+                COORDS+=("$WORD:$ROW:$COL:NE")
 
                 for ((J=0;J<${#WORD};J++)) {
                     CROW=$(($ROW-$J))
@@ -272,9 +365,9 @@ function put_words () {
                 ;;
 
             3)  #Diagonal SO
-                echo -n 'ORIENTATION: [\] (SO). - '
-                echo -n "ROW: $ROW - "
-                echo "COL: $COL"
+                showInfo "    Orientation: [\] (SO) - row: $ROW - col: $COL"
+
+                COORDS+=("$WORD:$ROW:$COL:SO")
 
                 for ((J=0;J<${#WORD};J++)) {
                     CROW=$(($ROW-$J))
@@ -301,34 +394,34 @@ function showHelp() {
 
 cat <<'EOF'
 
-                  /--                 --\
-                  | G P Q Z I Y S J R B |
-                  | O A V V N U K U G L |
-                  | C U V V X M I I L M |
-                  | E P V Z D O M E U Y |
-                  | Z Y V E H I T T V B |
-                  | D E E C H T P Y H B |
-                  | H H D K E F D A F H |
-                  | E K T R U W F Z R S |
-                  | U O D I S O U P G B |
-                  | F D J V F E I Z P N |
-                  \--                 --/
+                       /--                 --\
+                       | G P Q Z I Y S J R B |
+                       | O A V V N U K U G L |
+                       | C U V V X M I I L M |
+                       | E P V Z D O M E U Y |
+                       | Z Y V E H I T T V B |
+                       | D E E C H T P Y H B |
+                       | H H D K E F D A F H |
+                       | E K T R U W F Z R S |
+                       | U O D I S O U P G B |
+                       | F D J V F E I Z P N |
+                       \--                 --/
 
      :: Letter Soup Plugin for AEGS :: By pablo.niklas@gmail.com ::
 
 USAGE:
 ~~~~~~
 
-aegs.sh     -help:             This help.
-            -create-soup       Create the letter soup.
-                -size: size (nxn)
-                -words: list of words, separated by comma (,).
+lettersoup.sh  -help:           This help.
+               -create-soup:    Create the letter soup.
+                   -size:             Size (nxn).
+                   -words:            List of words, separated by comma (,).
+                  [-file <filename>]: Write the solution to a given file.
+                  [-print-soup]:      Print the generated soup.
 
 EOF
 
     exit 0
-
-
 }
 
 #######################################################
@@ -338,6 +431,7 @@ EOF
 [ $# -eq 0 ] && showHelp && exit 1
 
 # ARG processing
+FLAG_PS=0; FLAG_PT=0; FLAG_LA=0
 while [ ! -z "$1" ]; do
     case "x$1" in 
         "x-create-soup")   shift
@@ -352,12 +446,31 @@ while [ ! -z "$1" ]; do
                                             shift
                                             WORDS="$1"
                                             ;;
+
+                                "x-file")
+                                            shift
+                                            FILENAME="$1"
+                                            ;;
+
+                                "x-print-soup")
+                                            FLAG_PS=1
+                                            ;;
+
+                                "x-print-solution")
+                                                    FLAG_PT=1
+                                                    ;;
+
+                                "x-to-latex")   FLAG_LA=1
+                                                ;;
                             esac
                             shift
                         done
                         generate_soup
                         put_words $WORDS
-                        print_soup 
+                        [ $FLAG_PS -eq 1 ] && print_soup 
+                        [ $FLAG_PT -eq 1 ] && print_solution
+                        [ $FLAG_LA -eq 1 ] && soup2latex
+                        [ ! -z "$FILENAME" ] && coords2file "$FILENAME"
                         ;;
 
         "x-help")       showHelp ;;
@@ -366,4 +479,6 @@ while [ ! -z "$1" ]; do
     esac
     shift
 done 
+
+exit 0
 
